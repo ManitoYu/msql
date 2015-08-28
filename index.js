ad76026ce6fda1b48411c1daaad124d9ddade936
Onionkings <412950798@qq.com>
2015-08-28 19:30:20 +0800

    update delete limit

--- index.js
+++ index.js
@@ -1,37 +1,163 @@
-var utils = require('./lib/utils');
-
 var configAllowed = ['pk'];
 var configStorage = {};
 
-function Msql(table) {
+var restriction = {
+    pairs: {
+        field: '*',
+    },
+    setTable: function (table) {
+        this.pairs.table = table;
+    },
+    setWhere: function (where) {
+        this.pairs.where = where ? ' where ' + where : '';
+    },
+    setField: function (fields) {
+        this.pairs.field = fields.join(', ');
+    },
+    setCreate: function (createObj) {
+        this.pairs.createKey = createObj.createKey;
+        this.pairs.createValue = createObj.createValue;
+    },
+    setLimit: function () {
+        var args = arguments[0];
+        switch (args.length) {
+            case 1:
+                this.pairs.limit = ' LIMIT ' + args[0];
+                break;
 
+            case 2:
+                this.pairs.limit = ' LIMIT ' + args[0] + ', ' + args[1];
+                break;
 
-    var config = configStorage[table] || {};
-
-    var restriction = {
-        pairs: {
+            default:
+                throw new Error('limit may exists paramters error');
+        }
+    },
+    setKeyAndValue: function (kv) {
+        this.pairs.keyAndValue = kv;
+    },
+    clear: function (table) {
+        this.pairs = {
             field: '*',
             table: table
-        },
-        setWhere: function (where) {
-            this.pairs.where = where ? ' where ' + where : '';
-        },
-        setField: function (fields) {
-            this.pairs.field = fields.join(', ');
-        },
-        setCreate: function (createObj) {
-            this.pairs.createKey = createObj.createKey;
-            this.pairs.createValue = createObj.createValue;
+        };
+    }
+};
+
+var sqlTemplate = {
+
+    select: 'SELECT {field} FROM {table}{where}{limit}',
+    create: 'INSERT INTO {table}{createKey} VALUES {createValue}',
+    update: 'UPDATE {table} SET {keyAndValue}{where}{limit}',
+    delete: 'DELETE FROM {table}{where}'
+
+};
+
+var utils = {
+
+    /**
+     * 编译sql语句
+     *
+     * @param sqlTemp
+     * @returns {*}
+     */
+    buildSql: function (sqlTemp) {
+        var sql = sqlTemp;
+        var rep = restriction.pairs;
+        // 遍历传入的约束条件
+        Object.keys(rep).forEach(function (key) {
+            sql = sql.replace(new RegExp('{' + key + '}', 'g'), rep[key]);
+        });
+        sql = sql.replace(/{\w+}/ig, '');
+        restriction.clear(rep.table);
+        return sql;
+    },
+
+    /**
+     * 编译where条件
+     *
+     * @param field
+     * @param obj
+     * @returns {string}
+     */
+    compileWhere: function (field, obj) {
+
+        // 比较操作
+        var _condCompareObj = {
+            $eq: ' = ',
+            $neq: ' <> ',
+            $gt: ' > ',
+            $egt: ' >= ',
+            $lt: ' < ',
+            $elt: ' <= '
+        };
+        var _condCompareKeys = Object.keys(_condCompareObj);
+
+        var condType = Object.keys(obj)[0];
+        var condValue = obj[condType];
+        var condReturn;
+
+    //  console.log(field, condType, obj[condType]);
+
+        switch (true) {
+            case _condCompareKeys.indexOf(condType) != -1:
+                condReturn = field + _condCompareObj[condType] + condValue;
+                break;
+
+            case condType == '$in':
+                condValue = condValue.map(function (cv) {
+                    if (typeof cv == 'string') {
+                        return '\"' + escape(cv) + '\"';
+                    } else {
+                        return cv;
+                    }
+                });
+                condReturn =  field + ' in ' + '(' + condValue.join(', ') + ')';
+                break;
+
+            case condType == '$like':
+                // 单个like查询
+                if (typeof condValue != 'object') {
+                    return field + ' like \"' + condValue + '\"';
+                }
+                // 多个like查询
+                condReturn = condValue.map(function (itemValue) {
+                    return field + ' like \"' + itemValue + '\"';
+                }).join(' OR ');
+                break;
+
+            default:
+                throw new Error('not allowed query type: ' + condType);
         }
-    };
+        return '(' + condReturn + ')';
+    },
 
-    var sqlTemplate = {
+    /**
+     * 单双引号过滤
+     *
+     * @param value
+     * @returns {string}
+     */
+    escape: function (value) {
+        return '\"' + value.replace(/([\'\"])/g, '\\$1') + '\"';
+    },
 
-        select: 'SELECT {field} FROM {table}{where}',
-        create: 'INSERT INTO {table}{createKey} VALUES {createValue}'
+}
 
-    };
+function Msql(table) {
 
+    // 获取配置
+    var config = configStorage[table] || {};
+
+    // 设置当前活动表
+    restriction.setTable(table);
+
+    /**
+     * 配置
+     *
+     * @param conf
+     * @returns {*}
+     */
     Msql.config = function (conf) {
 
         if (! conf) {
@@ -48,12 +174,100 @@ function Msql(table) {
         return Msql(table);
 
     };
-    
-    // select more than one recoreds
+
+    /**
+     * 查询多条记录
+     *
+     * @returns {*}
+     */
     Msql.select = function () {
         return utils.buildSql(sqlTemplate.select, restriction.pairs);
     };
 
+    /**
+     * 查询单条记录
+     *
+     * @param query
+     * @returns {*}
+     */
+    Msql.find = function (query) {
+        if (typeof query != 'object') {
+            if (! config['pk']) {
+                throw new Error('you may forget set the field served as primary key');
+            }
+            restriction.setWhere(config['pk'] + ' = ' + query);
+        } else {
+            var field = Object.keys(query)[0];
+            restriction.setWhere(field + ' = ' + query[field]);
+        }
+        restriction.setLimit({ 0: 1 });
+        return utils.buildSql(sqlTemplate.select);
+    };
+
+    /**
+     * 创建新记录
+     *
+     * @param newObj
+     * @returns {*}
+     */
+    Msql.create = function (newObj) {
+        // 获取值列表
+        var values = Object.keys(newObj).map(function (field) {
+            if (typeof newObj[field] == 'string') {
+                return utils.escape(newObj[field]);
+            }
+            return newObj[field];
+        });
+        restriction.setCreate({
+            createKey: '(' + Object.keys(newObj).join(', ') + ')',
+            createValue: '(' + values.join(', ') + ')'
+        });
+        return utils.buildSql(sqlTemplate.create);
+    };
+
+    /**
+     * 更新记录
+     *
+     * @param query
+     * @returns {*}
+     */
+    Msql.update = function (query) {
+        if (! restriction.pairs.where) {
+            restriction.setWhere(config['pk'] + ' = ' + query['id']);
+            delete query['id'];
+        }
+        var kv = Object.keys(query).map(function (item) {
+            var value = query[item];
+            return item + ' = ' + (typeof value == 'string' ? utils.escape(value) : value);
+        }).join(', ');
+        restriction.setKeyAndValue(kv);
+
+        return utils.buildSql(sqlTemplate.update);
+    };
+
+    /**
+     * 删除记录
+     * @param query
+     * @returns {*}
+     */
+    Msql.delete = function (query) {
+        if (typeof query != 'object') {
+            restriction.setWhere(config['pk'] + ' = ' + query);
+        }
+        if (typeof query == 'object') {
+            var field = Object.keys(query)[0];
+            var value = query[field];
+            restriction.setWhere(field + ' = ' + (typeof value == 'string' ? utils.escape(value) : value));
+        }
+        return utils.buildSql(sqlTemplate.delete);
+    };
+
+    /**
+     * where限制
+     *
+     * @param where
+     * @returns {Msql}
+     */
     Msql.where = function (where) {
         var andWhere, orWhere, _or;
 
@@ -80,7 +294,7 @@ function Msql(table) {
 
             // 字符串
             if (typeof value == 'string') {
-                finalWhere[field] = field + ' = \"' + utils.escape(value) + '\"';
+                finalWhere[field] = field + ' = ' + utils.escape(value);
             }
 
             // 数字
@@ -94,12 +308,12 @@ function Msql(table) {
                 if (Array.isArray(value)) {
                     // 遍历每一个条件
                     finalWhere[field] = '(' + value.map(function (itemValue) {
-                        return utils.compileWhere(field, itemValue);
-                    }).join(' OR ') + ')';
+                            return utils.compileWhere(field, itemValue);
+                        }).join(' OR ') + ')';
                 }
                 // 对象
                 else {
-                     finalWhere[field] = utils.compileWhere(field, value);
+                    finalWhere[field] = utils.compileWhere(field, value);
                 }
             }
 
@@ -134,53 +348,26 @@ function Msql(table) {
         return Msql;
     };
 
+    /**
+     * 字段限制
+     *
+     * @param fields
+     * @returns {Msql}
+     */
     Msql.field = function (fields) {
         restriction.setField(fields);
         return Msql;
     };
 
-    // find only one record
-    Msql.find = function (query) {
-        if (typeof query != 'object') {
-            if (! config['pk']) {
-                throw new Error('you may forget set the field served as primary key');
-            }
-            restriction.setWhere(config['pk'] + ' = ' + query);
-        } else {
-            var field = Object.keys(query)[0];
-            restriction.setWhere(field + ' = ' + query[field]);
-        }
-        return utils.buildSql(sqlTemplate.select, restriction.pairs);
-    };
-
-    // create a new record
-    Msql.create = function (newObj) {
-        // 获取值列表
-        var values = Object.keys(newObj).map(function (field) {
-            if (typeof newObj[field] == 'string') {
-                return '\"' + utils.escape(newObj[field]) + '\"';
-            }
-            return newObj[field];
-        });
-        restriction.setCreate({
-            createKey: '(' + Object.keys(newObj).join(', ') + ')',
-            createValue: '(' + values.join(', ') + ')'
-        });
-        return Msql.buildSql(sqlTemplate.create);
-    };
-
-    // update one or more record
-    Msql.update = function (query) {
-        
-    };
-
-    // delete one or more record
-    Msql.delete = function (query) {
-        
-    };
+    /**
+     * 查询条数限制
+     */
+    Msql.limit = function () {
+        restriction.setLimit(arguments);
+        return Msql;
+    }
 
     return Msql;
-
 }
 
 module.exports = Msql;
--- readme.md
+++ readme.md
@@ -1,12 +1,9 @@
 ## Msql
 
 This is a tool for building sql without any stress.  
-
 With this tool, you can build a sql only by some paramters.  
-
 If you love this tool, I hope you can star me at my github [ManitoYu](https://github.com/ManitoYu/msql).  
-
-I am a chinese college student, and I want to try my best to contribute my code to the code world. So I hope you can support me.  
+I am a chinese college student, and I want to try my best to contribute my code to the code world. So I hope you can support me.    
 
 ### Select
 
@@ -21,20 +18,6 @@ var where = {
 };
 var sql = Msql('img').where(where).select();
 console.log(sql); // select * from img where id = 1 OR path = "images/3.jpg"
-```
-
-### Create
-
-Then you can see the demo referring to create a new record as follow.  
-
-```js
-var Msql = require('msql');
-var data = {
-    path: 'path1',
-    con: 'This is content'
-};
-var sql = Msql('img').create(data);
-console.log(sql); // INSERT INTO img(path, con) VALUES ("path1", "This is a new record")
 ```
 
 ### Find
@@ -56,8 +39,76 @@ var sql = Msql('img').find({ id: 1 });
 console.log(sql); // SELECT * FROM img where id = 1
 ```
 
-### Field
+### Create
 
+Then you can see the demo referring to create a new record as follow.  
+
+```js
+var Msql = require('msql');
+var data = {
+    path: 'path1',
+    con: 'This is content'
+};
+var sql = Msql('img').create(data);
+console.log(sql); // INSERT INTO img(path, con) VALUES ("path1", "This is a new record")
+```
+
+### Update
+
+Once you set assign the primay key of table, you can update a record as follow.  
+Assumed that you will update a record which id is equal to 1, you can do it like this.  
+
+```js
+var Img = Msql('img').config({
+    pk: 'id'
+});
+var sql = Img.update({ id: 1, path: 'path1' }); // specify the value of id, will update the record which id is 1
+console.log(sql); // UPDATE img SET path = "path1" where id = 1
+```
+
+### Delete
+
+if you specify the primary key, you can delete a record only by giving the value of pk.  
+
+```js
+var Img = Msql('img').config({
+    pk: 'id'
+});
+var sql = Img.delete(1); // id = 1
+console.log(sql); // DELETE FROM img where id = 1
+```
+
+And you can also delete it without any config.  
+
+```js
+var sql = Img.delete({ id: 1 }); 
+console.log(sql); // DELETE FROM img where id = 1
+```
+
+### Where
+
+* eq
+* neq
+* neq
+* gt
+* egt
+* lt
+* elt
+
+```js
+Img.where({ id: { $lt: 2 } }).select(); // SELECT * FROM img where (id < 2)
+Img.where({ id: [{ $gt: 1 }, { elt: 4 }] }).select(); // SELECT * FROM img where ((id > 1) OR (id <= 4))
+```
+
+* like
+* in
+
+```js
+Img.where({ path: { $like: 'con1' }, id: 3 }).select(); // SELECT * FROM img where (path like "con1") AND (id = 3)
+Img.where({ id: { $in: [1, 2, 3, 4] } }).select(); // SELECT * FROM img where (id in (1, 2, 3, 4))
+```
+
+### Field
 If you don't want to query all of fields, you can assign the several fields by yourself.  
 
 ```js
